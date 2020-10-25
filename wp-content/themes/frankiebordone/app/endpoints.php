ed444917490fcc9c13e31c41a8f934f379d4272a
Frankie Bordone <bordone.francesco@gmail.com>
2020-10-25 14:01:43 -0400

    Build out the home page

--- wp-content/themes/frankiebordone/app/endpoints.php
+++ wp-content/themes/frankiebordone/app/endpoints.php
@@ -17,12 +17,21 @@ class Endpoints {
         register_rest_route($this->namespace, 'menu', [
             'methods' => 'GET',
             'callback' => [$this, 'get_wp_nav_menu'],
+            'permission_callback' => '__return_true',
         ]);
 
         // expose nav related ACF data to custom endpoint called 'nav'
         register_rest_route($this->namespace, 'nav', [
             'methods' => 'GET',
             'callback' => [$this, 'get_nav_acf'],
+            'permission_callback' => '__return_true',
+        ]);
+
+        // expose home page related ACF data to custom endpoint called 'home'
+        register_rest_route($this->namespace, 'home', [
+            'methods' => 'GET',
+            'callback' => [$this, 'get_home_acf'],
+            'permission_callback' => '__return_true',
         ]);
     }
 
@@ -52,13 +61,33 @@ class Endpoints {
     public function get_nav_acf() {
         $nav_acf_data = [];
 
-        if ($nav_image = get_field('nav__image', 'option')) {
+        if ($nav_image = get_field('options__image', 'option')) {
             $nav_acf_data = [
                 'image' => $nav_image
             ];
         }
 
         return $nav_acf_data;
+    }
+
+    /*
+     * Helper function for custom endpoint 'home'
+     */
+    public function get_home_acf() {
+        $home_acf_data = [];
+        $home_page_id = get_field('options__home_page_id', 'option');
+
+        if ($home_title = get_field('home__title', $home_page_id)) {
+            $home_acf_data = [
+                'title' => $home_title
+            ];
+        }
+
+        if ($home_tagline = get_field('home__tagline', $home_page_id)) {
+            $home_acf_data['tagline'] = $home_tagline;
+        }
+
+        return $home_acf_data;
     }
 }
 
