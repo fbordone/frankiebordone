4b1eb1c6881b1636f8248fc34b13834ae53a8ad8
Frankie Bordone <bordone.francesco@gmail.com>
2020-11-04 01:49:07 -0500

    Bake out the projects page

--- wp-content/themes/frankiebordone/resources/assets/scripts/react/components/Projects.js
+++ wp-content/themes/frankiebordone/resources/assets/scripts/react/components/Projects.js
@@ -1,10 +1,56 @@
 import React from 'react';
+import axios from 'axios';
+
+import BackgroundDots from './BackgroundDots';
 
 class Projects extends React.Component {
+  state = {
+    projects: [],
+    visible: false,
+  };
+
+  getPageContent() {
+    axios.get('/wp-json/wp/v2/projects')
+      .then(res => {
+        this.setState({
+          projects: res.data,
+          visible: true,
+        });
+      })
+  }
+
+  componentDidMount() {
+    this.getPageContent();
+  }
+
   render() {
     return (
       <div className="projects">
-        <p>This is a projects!</p>
+        <BackgroundDots />
+
+        <div className={ `padding-top ${this.state.visible ? 'visible' : 'hidden'}` }>
+          <div className="projects__wrap">
+            <h2 className="projects__section_title">Projects</h2>
+
+            <div className="projects__grid">
+              {this.state.projects.map(project =>
+                <div key={project.ID} className="projects__grid_card">
+                  <div className="projects__grid_card_content">
+                    <a href={project.link} target="_blank">
+                      <img className="projects__grid_card_img" src={project.image} alt={project.title} />
+                    </a>
+
+                    <a href={project.link} target="_blank">
+                      <h4 className="projects__grid_card_title">{project.title}</h4>
+                    </a>
+
+                    <p className="projects__grid_card_copy">{project.tagline}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
