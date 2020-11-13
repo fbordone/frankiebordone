1aa1cb8187c9c792ebfad1ee267b07e4cb4ce054
Frankie Bordone <bordone.francesco@gmail.com>
2020-11-13 16:11:31 -0500

    Favicon - take 3

--- wp-content/themes/frankiebordone/resources/views/index.blade.php
+++ wp-content/themes/frankiebordone/resources/views/index.blade.php
@@ -6,11 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     {{-- Favicon --}}
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ home_url() }}/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ home_url() }}/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ home_url() }}/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ home_url() }}/favicon-16x16.png">
     <link rel="manifest" href="{{ home_url() }}/site.webmanifest">
-    <link rel="mask-icon" href="{{ home_url() }}/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
